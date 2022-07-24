b4aa8ed63f5b0c91fd6efdbe5f578991abcd5b12
Abhishek Bhatt <abhi.bhatt100@gmail.com>
2022-07-24 18:50:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char unicodeChar= '\u0044';
+        System.out.println(myChar);
+        System.out.println(unicodeChar);
+
+        char unicodeChar2= '\u00A9';
+        System.out.println(unicodeChar2);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
